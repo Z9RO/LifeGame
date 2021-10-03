e951b1db9968466f688d9cce2e7a8cec39eb2b45
z9ro@msi.debian <z9ro4github@outlook.com>
2021-10-03 10:41:10 +0800

    Remove unnecessary check

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,4 @@
 pub struct Board {
-    row: usize,
     col: usize,
     values: Vec<u8>,
 }
@@ -7,25 +6,15 @@ pub struct Board {
 impl Board {
     pub fn new(row: usize, col: usize) -> Board {
         Board {
-            row,
             col,
             values: vec![0; row * col],
         }
     }
-    pub fn set(&mut self, x: usize, y: usize, value: u8) -> Result<(), &'static str> {
-        if x >= self.row || y >= self.col {
-            Err("Out of bounds")
-        } else {
-            self.values[y + x * self.col] = value;
-            Ok(())
-        }
+    pub fn set(&mut self, x: usize, y: usize, value: u8) {
+        self.values[y + x * self.col] = value;
     }
-    pub fn get(&self, x: usize, y: usize) -> Option<u8> {
-        if x >= self.row || y >= self.col {
-            None
-        } else {
-            Some(self.values[y + x * self.col])
-        }
+    pub fn get(&self, x: usize, y: usize) -> u8 {
+        self.values[y + x * self.col]
     }
 }
 
@@ -41,7 +30,7 @@ mod test {
 
         for x in 0..row {
             for y in 0..col {
-                assert_eq!(Some(0), one_board.get(x, y));
+                assert_eq!(0, one_board.get(x, y));
             }
         }
     }
@@ -55,30 +44,8 @@ mod test {
         let value = 9;
         let x = 2;
         let y = 1;
-        let res = one_board.set(x, y, value);
+        one_board.set(x, y, value);
 
-        assert_eq!(Ok(()), res);
-        assert_eq!(Some(value), one_board.get(x, y));
-    }
-
-    #[test]
-    fn board_set_failed() {
-        let row = 3;
-        let col = 4;
-        let mut one_board = Board::new(row, col);
-
-        let res = one_board.set(row, col - 1, 3);
-
-        assert_eq!(Err("Out of bounds"), res);
-    }
-
-    #[test]
-    fn board_get_failed() {
-        let row = 3;
-        let col = 4;
-        let one_board = Board::new(row, col);
-
-        let res = one_board.get(row, col - 1);
-        assert_eq!(None, res);
+        assert_eq!(value, one_board.get(x, y));
     }
 }
